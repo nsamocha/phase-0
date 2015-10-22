585415d3c00d7fb260a987df13b605d3e270f67f
Nimi Samocha <nimisamocha@gmail.com>
2015-10-22 16:12:26 -0700

    4.5.1

---
+++ good-guess/my_solution.rb
@@ -0,0 +1,13 @@
+# Good Guess
+
+# I worked on this challenge [by myself, with: ].
+def good_guess?(x)
+  if x == 42
+    return true
+  else
+    return false
+  end
+  good_guess?(42)
+
+# Your Solution Below
+end
